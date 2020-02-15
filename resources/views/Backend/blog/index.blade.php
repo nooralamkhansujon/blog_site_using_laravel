7c929a6f8b9314737935d78d7fdebd070c75f87e
Nooralam khan <CoreyMShafer@gmail.com>
2020-02-15 11:28:53 +0600

    image column addded in blogs table

--- resources/views/Backend/blog/index.blade.php
+++ resources/views/Backend/blog/index.blade.php
@@ -1,5 +1,20 @@
 @extends('backend.app')
 
 @section('content')
-This is our dashboard
-@endsection
+    <div class="row">
+         <div class="col-md-12">
+             <h1>Blog List</h1>
+         </div>
+         <div class="col-md-12">
+               <table class="table table-hover table-responsive-md">
+                 <tr>
+                     <th>Sr</th>
+                     <th></th>
+                     <th></th>
+                     <th></th>
+                     <th></th>
+                 </tr>
+
+              </table>
+         </div>
+    </div>
