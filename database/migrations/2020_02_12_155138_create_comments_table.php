1a42a052e279f89805e1b78a01590014b331ae2f
Nooralam khan <CoreyMShafer@gmail.com>
2020-02-23 10:10:47 +0600

    commentable_id, commentable_type and comments column added in comments table

--- database/migrations/2020_02_12_155138_create_comments_table.php
+++ database/migrations/2020_02_12_155138_create_comments_table.php
@@ -16,7 +16,9 @@ class CreateCommentsTable extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id');
-            $table->integer('blog_id');
+            $table->integer('commentable_id');
+            $table->integer('commentable_type');
+            $table->text('comment');
             $table->integer('parent_id')->default(0);
             $table->timestamps();
             $table->softDeletes();
